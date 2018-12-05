058503decd8559099646bd548e1f6a6b6a4bdccf
Christine Tham <chris.tham@hellotham.com>
2018-12-05 14:02:02 +1100

    Initial Commit

---
+++ myStatus365/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  myStatus365
+//
+//  Created by Chris Tham on 5/12/18.
+//  Copyright © 2018 Transport for NSW. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ myStatus365/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myStatus365
+//
+//  Created by Chris Tham on 5/12/18.
+//  Copyright © 2018 Transport for NSW. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
